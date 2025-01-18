1dabfbbe114879d7b8488457e3942579dce320ef
Bruce Neco <bruce.neco@gmail.com>
2025-01-18 18:26:38 -0300

    docs: word entities

--- word.go
+++ word.go
@@ -1,15 +1,28 @@
 package brwords
 
+// Meaning is the
 type Meaning struct {
 	Tag, Content string
 }
+
+// Phrase is an example of word usage.
 type Phrase struct {
-	Content, By string
+	// Content is the phrase itself.
+	Content string
+	// By is the source of the Phrase.
+	By string
 }
+
+// Word is the core result of an extraction.
 type Word struct {
-	Content     string
-	Meanings    []Meaning
-	Synonyms    []string
+	// Content is the word itself
+	Content string
+	// Meanings are a list of possible meanings
+	Meanings []Meaning
+	// Synonyms are a list of possible synonyms
+	Synonyms []string
+	// Etymologies are a list of possible etymologies
 	Etymologies []string
-	Phrases     []Phrase
+	// Phrases are a list of example phrases
+	Phrases []Phrase
 }
