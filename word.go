d91a24077b53ecb5f5d92f564b30f22e29d2b913
Bruce Neco <bruce.neco@gmail.com>
2025-01-25 21:36:57 -0300

    feat: add json annotation to word entity

--- word.go
+++ word.go
@@ -1,28 +1,31 @@
 package brwords
 
-// Meaning is the
+// Meaning is the definition of a word
 type Meaning struct {
-	Tag, Content string
+	// Tag is the type of meaning
+	Tag string `json:"tipo,omitempty"`
+	// Content is the meaning itself
+	Content string `json:"conteudo"`
 }
 
 // Phrase is an example of word usage.
 type Phrase struct {
 	// Content is the phrase itself.
-	Content string
+	Content string `json:"conteudo"`
 	// By is the source of the Phrase.
-	By string
+	By string `json:"autor"`
 }
 
 // Word is the core result of an extraction.
 type Word struct {
 	// Content is the word itself
-	Content string
+	Content string `json:"palavra"`
 	// Meanings are a list of possible meanings
-	Meanings []Meaning
+	Meanings []Meaning `json:"significados"`
 	// Synonyms are a list of possible synonyms
-	Synonyms []string
+	Synonyms []string `json:"sinonimos"`
 	// Etymologies are a list of possible etymologies
-	Etymologies []string
+	Etymologies []string `json:"etimologias"`
 	// Phrases are a list of example phrases
-	Phrases []Phrase
+	Phrases []Phrase `json:"frases"`
 }
